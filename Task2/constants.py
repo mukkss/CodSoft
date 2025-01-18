eb97321e5fb2517ae2aa0a941d11848b405a607e
mukkss <muksss1102@gmail.com>
2025-01-18 18:35:50 +0530

    updates constants

---
+++ Task2/constants.py
@@ -0,0 +1,26 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- BOARD DIMENSIONS ---
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS  # Size of each square
+
+# --- LINE, CIRCLE, AND CROSS SIZES ---
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4  # Circle radius
+
+OFFSET = 50  # Offset for drawing
+
+# --- COLORS ---
+BG_COLOR = (28, 170, 156)  # Background color
+LINE_COLOR = (23, 145, 135)  # Line color
+CIRC_COLOR = (239, 231, 200)  # Circle color
+CROSS_COLOR = (66, 66, 66)  # Cross color
